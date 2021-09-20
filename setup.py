a36e32c0888dd39a887e69f4d209983ff174795a
JoinChang <joinchang1206@gmail.com>
2021-09-20 11:05:05 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,5 +7,11 @@ setup(
     author_email="joinchang1206@gmail.com",
     description="一款为 go-cqhttp 的正向 WebSocket 设计的 Python SDK，支持纯 CQ 码与消息链的转换处理",
     url="https://github.com/Lxns-Network/nakuru-project",
-    packages=find_packages()
+    packages=find_packages(include=("nakuru", "nakuru.*")),
+    install_requires=[
+        "aiohttp",
+        "pydantic",
+        "Logbook",
+        "async_lru"
+    ]
 )
