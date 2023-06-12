2bbde8bcc74e76fa8ef607b9071acf122a164eaa
JoinChang <35451392+JoinChang@users.noreply.github.com>
2023-06-12 20:51:41 +0800

    v1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="nakuru-project",
-    version="1.0.0",
+    version="1.0.1",
     author="Lxns-Network",
     author_email="joinchang1206@gmail.com",
     description="一款为 go-cqhttp 的正向 WebSocket 设计的 Python SDK，支持纯 CQ 码与消息链的转换处理",
